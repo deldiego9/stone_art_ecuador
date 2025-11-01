475ece846229c844943bfbecaa19a1415b54371c
Diego Paredes <deldiego9.es@gmail.com>
2025-11-01 15:28:32 +0100

    Actualizar app.py con envío a 2 correos y listo para Render

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app.secret_key = os.environ.get("SECRET_KEY", "clave_temporal")  # Para mensajes
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
 app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'deldiego9.es@gmail.com')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'qeqhqlofdejntmvb')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', 'deldiego9.es@gmail.com')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 
 mail = Mail(app)
 
