9904f8c8640a83f1232dafc1dfd7e4e5be98a573
Diego Paredes <deldiego9.es@gmail.com>
2025-11-01 15:49:15 +0100

    Corregir configuración Flask-Mail con variables de entorno

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "clave_temporal")  # Para mensajes flash
 
 # ---------------- Configuración Flask-Mail ----------------
+# ---------------- Configuración Flask-Mail ----------------
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
 app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', os.environ.get('MAIL_USERNAME'))
 
 mail = Mail(app)
 
